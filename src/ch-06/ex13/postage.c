00a921d767fc4299ed77ee39133401033eda69a1
liyinyi278 <liyinyi278@163.com>
2025-11-03 19:36:57 +0800

    第06章：C控制语句-循环_6.4~6.7

---
+++ src/ch-06/ex13/postage.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  const int FIRST_OZ = 46; // 2013邮资
+  const int NEXT_OZ = 20;  // 2013邮资
+  int ounces, cost;
+
+  printf(" ounces: cost\n");
+  for (ounces = 1, cost = FIRST_OZ; ounces <= 16; ounces++, cost += NEXT_OZ)
+    printf("%7d  $%-6.2f\n", ounces, cost / 100.0);
+
+  return 0;
+}
