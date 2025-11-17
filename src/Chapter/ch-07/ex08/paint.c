c2ae04002ae137184a7997e758dbfecf77e2b117
liyinyi278 <liyinyi278@163.com>
2025-11-17 14:36:18 +0800

    第07章：C控制语句-分支和跳转_7.5&7.6

---
+++ src/Chapter/ch-07/ex08/paint.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  const int COVERAGE = 350; //每罐油漆可刷的面积（单位：平方英尺）
+
+  int sq_feet, cans;
+
+  printf("Enter the number of square feet to be painted:\n");
+  while (scanf("%d", &sq_feet) == 1) {
+    cans = sq_feet / COVERAGE;
+    cans += ((sq_feet % COVERAGE) == 0) ? 0 : 1;
+    printf("You need %d %s of paint.\n", cans, cans == 1 ? "can" : "cans");
+    printf("Enter next value (q to quit):\n");
+  }
+
+  return 0;
+}
