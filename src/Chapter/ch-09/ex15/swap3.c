539274a56f87589bec7e5011aa81432519deb491
liyinyi278 <liyinyi278@163.com>
2025-12-14 10:57:22 +0800

    第09章-函数：9.7-指针简介

---
+++ src/Chapter/ch-09/ex15/swap3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void inter_change(int *u, int *v);
+
+int main(void) {
+  int x = 5, y = 10;
+
+  printf("Originally x = %d, y = %d.\n", x, y);
+  inter_change(&x, &y);
+  printf("Now x = %d, y = %d.\n", x, y);
+
+  return 0;
+}
+
+void inter_change(int *u, int *v) {
+  int temp;
+
+  temp = *u;
+  *u = *v;
+  *v = temp;
+}
